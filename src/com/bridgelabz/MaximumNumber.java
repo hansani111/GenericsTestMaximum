8d3f4447648dcbda31ea887f1f1619ff35a2059b
hansani111 <hansanideshmukh894@gmail.com>
2023-01-16 16:24:29 +0530

    added: welcome message to master

---
+++ src/com/bridgelabz/MaximumNumber.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+        System.out.println("-----Test Maximum of 3 Variables-----");
+    }
+}
